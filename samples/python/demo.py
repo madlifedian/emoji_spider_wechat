f8dd2d7fef123d95d00e83c6754d2e9152bb1d06
Bug-Maker <863224479@qq.com>
2020-11-14 22:30:55 +0800

    注释添加

--- samples/python/demo.py
+++ samples/python/demo.py
@@ -1,30 +1,31 @@
 # -*- coding: utf-8 -*-
+
+# 把下一个新版本的特性导入到当前版本，就可以在当前版本中测试一些新版本的特性
 from __future__ import unicode_literals
-
+# python自带的操作文件的相关模块
 import glob
-
 import wechat
-import json
 import time
-from wechat import WeChatManager, MessageType
+# from wechat import WeChatManager, MessageType
 
-wechat_manager = WeChatManager(libs_path='../../libs')
+# 加载libs包中的hook文件
+wechat_manager = wechat.WeChatManager(libs_path='../../libs')
 
 imgs = []
 
-# 这里测试函数回调
+# 微信连接时函数回调
 @wechat.CONNECT_CALLBACK(in_class=False)
 def on_connect(client_id):
     print('[on_connect] client_id: {0}'.format(client_id))
 
-
+# 微信接收到消息时函数回调
 @wechat.RECV_CALLBACK(in_class=False)
 def on_recv(client_id, message_type, message_data):
     print('[on_recv] client_id: {0}, message_type: {1}, message:{2}'.format(client_id,
                                                                             message_type, message_data))
 
     # 如果发送文本信息 且 发送对象是文本传输助手
-    # if message_type == MessageType.MT_RECV_TEXT_MSG and message_data['to_wxid'] == "filehelper":
+    # if message_type == wechat.MessageType.MT_RECV_TEXT_MSG and message_data['to_wxid'] == "filehelper":
     #
     #     # 本地图片库中搜索图片
     #     img_text = message_data['msg']
@@ -37,7 +38,7 @@ def on_recv(client_id, message_type, message_data):
     #         print('开始发送表情： ', img)
     #     imgs.clear()
 
-    if message_type == MessageType.MT_RECV_TEXT_MSG and message_data['to_wxid'] == "wxid_ocjpv4fvhm8122":
+    if message_type == wechat.MessageType.MT_RECV_TEXT_MSG and message_data['to_wxid'] == "wxid_ocjpv4fvhm8122":
 
         # 本地图片库中搜索图片
         img_text = message_data['msg']
@@ -50,27 +51,27 @@ def on_recv(client_id, message_type, message_data):
             print('开始发送表情： ', img)
         imgs.clear()
 
-
+# 微信关闭时函数回调
 @wechat.CLOSE_CALLBACK(in_class=False)
 def on_close(client_id):
     print('[on_close] client_id: {0}'.format(client_id))
 
 
-# 这里测试类回调， 函数回调与类回调可以混合使用
+# 这里类回调， 函数回调与类回调可以混合使用
 class LoginTipBot(wechat.CallbackHandler):
 
     @wechat.RECV_CALLBACK(in_class=True)
     def on_message(self, client_id, message_type, message_data):
         # 判断登录成功后，就向文件助手发条消息
-        if message_type == MessageType.MT_USER_LOGIN:
+        if message_type == wechat.MessageType.MT_USER_LOGIN:
             time.sleep(2)
             wechat_manager.send_text(client_id, 'filehelper', '😂😂😂\uE052该消息通过wechat_pc_api项目接口发送')
 
             wechat_manager.send_link(client_id,
             'filehelper',
-            'wechat_pc_api项目',
-            'WeChatPc机器人项目',
-            'https://github.com/smallevilbeast/wechat_pc_api',
+            'hello~',
+            '微信robot~~',
+            'https://github.com/Bug-Maker/emoji_spider_wechat',
             'https://www.showdoc.com.cn/server/api/attachment/visitfile/sign/0203e82433363e5ff9c6aa88aa9f1bbe?showdoc=.jpg)')
 
 
--- samples/python/wechat.py
+++ samples/python/wechat.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
+"""
+微信hook原理：进程注入（逆向工程）。
+就是让你的目标软件无声的加载你自己编写的dll文件，然后运行，这样你的dll就变成微信程序的一部分，可以读取它的任何数据，
+来进行接下来的拦截过程。
+"""
 
 import json
 import sys
 import os
 import os.path
+# 获取类或者函数的参数信息
 import inspect
 import copy
 from functools import wraps
+
+# ctypes使用c函数，python和c的混合编程工具
 from ctypes import WinDLL, c_ulong, c_char_p, create_string_buffer, WINFUNCTYPE
 
 import logging
 
+# 对WeChatManager类进行log记录
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('WeChatManager')
 
-
+# 判断系统是64位还是32位
 def is_64bit():
     return sys.maxsize > 2 ** 32
 
@@ -24,6 +33,11 @@ def c_string(data):
 
 
 class MessageType:
+
+    """
+    通信接口类型表：具体参考：
+    https://www.showdoc.com.cn/579570325733136?page_id=3417087407035329
+    """
     MT_DEBUG_LOG = 11024
     MT_RECV_QRCODE_MSG = 11087
 
@@ -35,16 +49,22 @@ class MessageType:
     MT_DATA_CHATROOM_MEMBERS_MSG = 11032
     MT_DATA_PUBLICS_MSG = 11033
 
+    # 发送文本信息
     MT_SEND_TEXTMSG = 11036
     MT_SEND_CHATROOM_ATMSG = 11037
     MT_SEND_CARDMSG = 11038
+    # 发送链接信息
     MT_SEND_LINKMSG = 11039
+    # 发送图片信息
     MT_SEND_IMGMSG = 11040
     MT_SEND_FILEMSG = 11041
     MT_SEND_VIDEOMSG = 11042
+    # 发送gif信息
     MT_SEND_GIFMSG = 11043
 
+    # 接收文本信息
     MT_RECV_TEXT_MSG = 11046
+    # 接收图片信息
     MT_RECV_PICTURE_MSG = 11047
     MT_RECV_VOICE_MSG = 11048
     MT_RECV_FRIEND_MSG = 11049
@@ -70,7 +90,7 @@ _GLOBAL_CONNECT_CALLBACK_LIST = []
 _GLOBAL_RECV_CALLBACK_LIST = []
 _GLOBAL_CLOSE_CALLBACK_LIST = []
 
-
+# 微信连接时的装饰器
 def CONNECT_CALLBACK(in_class=False):
     def decorator(f):
         wraps(f)
@@ -82,7 +102,7 @@ def CONNECT_CALLBACK(in_class=False):
 
     return decorator
 
-
+# 微信收到消息时装饰器
 def RECV_CALLBACK(in_class=False):
     def decorator(f):
         wraps(f)
@@ -94,7 +114,7 @@ def RECV_CALLBACK(in_class=False):
 
     return decorator
 
-
+# 微信关闭时装饰器
 def CLOSE_CALLBACK(in_class=False):
     def decorator(f):
         wraps(f)
@@ -116,7 +136,7 @@ def add_callback_handler(callbackHandler):
         elif hasattr(handler, '_wx_close_handled'):
             _GLOBAL_CLOSE_CALLBACK_LIST.append(handler)
 
-
+# 使用c语言ctype模块调用dll接口
 @WINFUNCTYPE(None, c_ulong)
 def wechat_connect_callback(client_id):
     for func in _GLOBAL_CONNECT_CALLBACK_LIST:
@@ -125,6 +145,8 @@ def wechat_connect_callback(client_id):
 
 @WINFUNCTYPE(None, c_ulong, c_char_p, c_ulong)
 def wechat_recv_callback(client_id, data, length):
+
+    # json数据深拷贝，并将json对象转成
     data = copy.deepcopy(data)
     json_data = data.decode('utf-8')
     dict_data = json.loads(json_data)
@@ -137,7 +159,7 @@ def wechat_close_callback(client_id):
     for func in _GLOBAL_CLOSE_CALLBACK_LIST:
         func(client_id)
 
-
+# WxLoader.dll文件的装饰器
 class REQUIRE_WXLOADER:
     def __call__(self, func):
 
@@ -152,7 +174,7 @@ class REQUIRE_WXLOADER:
 
         return wrapper
 
-
+# dook的python管理程序
 class WeChatManager:
     # 加载器
     WXLOADER = None
@@ -165,6 +187,7 @@ class WeChatManager:
 
     def __init__(self, libs_path, wechat_exe_path=''):
         self.wechat_exe_path = wechat_exe_path
+        # 加载WxLoader.dll
         wechat_loader_path = os.path.join(libs_path, 'WxLoader_{0}.dll'.format('x64' if is_64bit() else 'x86'))
         wechat_loader_path = os.path.realpath(wechat_loader_path)
 
@@ -172,14 +195,16 @@ class WeChatManager:
             logger.error('libs path error or wechatLoader not exist: %s', wechat_loader_path)
             return
 
+        # 加载libs文件夹中的WxLoader.dll文件
         self.WXLOADER = WinDLL(wechat_loader_path)
 
         # 使用utf8编码
         self.WXLOADER.UseUtf8()
 
-        # 初始化接口回调
+        # 初始化接口回调，调用WxLoader.dll的InitWeChatSocket接口
         self.WXLOADER.InitWeChatSocket(wechat_connect_callback, wechat_recv_callback, wechat_close_callback)
 
+        # 加载WeChatHelper.dll（客户端程序，用于微信程序内部与外界通信，用于接收指令和消息通知）
         self.wxhelper_dll_path = '{0}/WeChatHelper_{1}.dll'.format(libs_path, self.get_user_wechat_version())
         if not os.path.exists(self.wxhelper_dll_path):
             logger.error('lib file：%s not exist', self.wxhelper_dll_path);
@@ -190,15 +215,18 @@ class WeChatManager:
 
         self.wechat_exe_path = wechat_exe_path
 
+    # 添加回溯句柄
     def add_callback_handler(self, callback_handler):
         add_callback_handler(callback_handler)
 
+    # 调用WxLoader.GetUserWeChatVersion，获取当前用户的电脑上安装的微信版本
     @REQUIRE_WXLOADER()
     def get_user_wechat_version(self):
         out = create_string_buffer(20)
         self.WXLOADER.GetUserWeChatVersion(out)
         return out.value.decode('utf-8')
 
+    # 调用WxLoader.InjectWeChat接口或者InjectWeChatMultiOpen接口，用于智能多开，并注入dll, 注入成功返回微信的进程ID, 失败返回0
     @REQUIRE_WXLOADER()
     def manager_wechat(self, smart=True):
         if smart:
@@ -206,14 +234,18 @@ class WeChatManager:
         else:
             return self.WXLOADER.InjectWeChatMultiOpen(c_string(self.wxhelper_dll_path), c_string(self.wechat_exe_path))
 
+    # 调用WxLoader.InjectWeChatPid接口，注入指定的微信进程
     @REQUIRE_WXLOADER()
     def manager_wechat_by_pid(self, wechat_pid):
         return self.WXLOADER.InjectWeChatPid(wechat_pid, c_string(self.wxhelper_dll_path))
 
+    # 调用WxLoader.DestroyWeChat接口，主程序退出前，执行释放函数，用于卸载DLL和关闭socket服务端
     @REQUIRE_WXLOADER()
     def close_manager(self):
         return self.WXLOADER.DestroyWeChat()
 
+    # 调用WxLoader.SendWeChatData接口，用于向微信发送指令
+    # 函数原型: BOOL SendWeChatData(IN CONNID dwClientId, IN LPCSTR szJsonData);
     @REQUIRE_WXLOADER()
     def send_message(self, client_id, message_type, params):
         send_data = {
@@ -232,7 +264,7 @@ class WeChatManager:
 
     # 发送群at消息
     def send_chatroom_at_msg(self, client_id, to_wxid, content, at_list):
-        data = { 
+        data = {
             'to_wxid': to_wxid,
             'content': content,
             'at_list': at_list
@@ -241,7 +273,7 @@ class WeChatManager:
 
     # 发送名片
     def send_user_card(self, client_id, to_wxid, wxid):
-        data = { 
+        data = {
             'to_wxid': to_wxid,
             'card_wxid': wxid
         }
@@ -272,7 +304,7 @@ class WeChatManager:
             'to_wxid': to_wxid,
             'file': file
         }
-        return self.send_message(client_id, MessageType.MT_SEND_FILEMSG, data)  
+        return self.send_message(client_id, MessageType.MT_SEND_FILEMSG, data)
 
     # 发送视频
     def send_video(self, client_id, to_wxid, file):
@@ -280,7 +312,7 @@ class WeChatManager:
             'to_wxid': to_wxid,
             'file': file
         }
-        return self.send_message(client_id, MessageType.MT_SEND_VIDEOMSG, data)     
+        return self.send_message(client_id, MessageType.MT_SEND_VIDEOMSG, data)
 
     # 发送gif
     def send_gif(self, client_id, to_wxid, file):
@@ -288,7 +320,7 @@ class WeChatManager:
             'to_wxid': to_wxid,
             'file': file
         }
-        return self.send_message(client_id, MessageType.MT_SEND_GIFMSG, data) 
+        return self.send_message(client_id, MessageType.MT_SEND_GIFMSG, data)
 
     # 获取所有联系人
     def get_friends(self, client_id):
